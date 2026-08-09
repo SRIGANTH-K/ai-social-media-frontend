78c00e304d82d9795d005d2ba3045948215ac1be
SRIGANTH-K <sriganthkofficial@gmail.com>
2026-08-09 20:48:28 +0530

    Add storage configuration for social media images

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,1 +1,10 @@
+import { defineStorage } from "@aws-amplify/backend";
 
+export const storage = defineStorage({
+  name: "socialMediaImages",
+  access: (allow) => ({
+    "images/*": [
+      allow.guest.to(["read", "write", "delete"]),
+    ],
+  }),
+});
